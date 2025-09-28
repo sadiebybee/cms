df0cc120c4cbc2cddb724e7b8710203a22cbd83f
sadiebybee <byb22004@byui.edu>
2025-09-27 22:32:48 -0600

    Cms2

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -7,12 +7,5 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent {
-  // Temporary dummy data to match the screenshot
-  contact: Contact = new Contact(
-    '1',
-    'R. Kent Jackson',
-    'jacksonk@byui.edu',
-    '208-496-3771',
-    '../../assets/images/jacksonk.jpg'
-  );
+  contact: Contact = {} as Contact;
 }
