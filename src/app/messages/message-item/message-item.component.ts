1001734ba4ec0a49f2b80c9e5aa1ec80c0ac1084
sadiebybee <byb22004@byui.edu>
2025-10-04 12:39:12 -0600

    message-item

--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-item',
   templateUrl: './message-item.component.html',
-  styleUrl: './message-item.component.css'
+  styleUrls: ['./message-item.component.css']
 })
 export class MessageItemComponent {
-
+  @Input() message!: Message;
 }
