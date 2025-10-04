032cd98dc735ecc226e898b122b5d6e582f951a3
sadiebybee <byb22004@byui.edu>
2025-10-04 13:00:27 -0600

    message-edit

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
   templateUrl: './message-edit.component.html',
-  styleUrl: './message-edit.component.css'
+  styleUrl: './message-edit.component.css',
 })
 export class MessageEditComponent {
+  @ViewChild('subjectInput') subjectInputRef!: ElementRef;
+  @ViewChild('msgTextInput') msgTextInputRef!: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Sadie';
 
+  onSendMessage() {
+    const subject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgTextInputRef.nativeElement.value;
+    const newMessage = new Message('1', subject, msgText, this.currentSender);
+
+    this.addMessageEvent.emit(newMessage);
+    this.onClear();
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
 }
