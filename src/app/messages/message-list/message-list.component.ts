8760a1b1d8af83383d7be064b1e12b1d898ba9ff
sadiebybee <byb22004@byui.edu>
2025-10-04 13:19:19 -0600

    Message-list

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
-  styleUrl: './message-list.component.css'
+  styleUrl: './message-list.component.css',
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message('1', 'Rent', 'Rent is due on the 1st of the month', 'Jane D.'),
+    new Message('2', 'Reminder', '1pm meeting this Tuesday', 'Sarah J.'),
+    new Message('3', 'Dinner', 'Dinner with Jane at 6pm', 'John D.'),
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
