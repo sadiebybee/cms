917b702ea13d5c20cb4ab4b19c908cf65f35cc10
sadiebybee <byb22004@byui.edu>
2025-10-23 20:12:29 -0600

    message

--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Message } from '../message.model';
+import { ContactService } from '../../contacts/contact.service';
+import { Contact } from '../../contacts/contact.model';
 
 @Component({
   selector: 'cms-message-item',
   templateUrl: './message-item.component.html',
-  styleUrls: ['./message-item.component.css']
 })
-export class MessageItemComponent {
-  @Input() message!: Message;
+export class MessageItemComponent implements OnInit {
+  @Input() message!: Message; // input message
+  messageSender: string = ''; // string to display sender's name
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    const contact: Contact | null = this.contactService.getContact(this.message.sender);
+    this.messageSender = contact ? contact.name : 'Unknown';
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
-  styleUrl: './message-list.component.css',
+  styleUrls: ['./message-list.component.css'],
 })
-export class MessageListComponent {
-  messages: Message[] = [
-    new Message('1', 'Rent', 'Rent is due on the 1st of the month', 'Jane D.'),
-    new Message('2', 'Reminder', '1pm meeting this Tuesday', 'Sarah J.'),
-    new Message('3', 'Dinner', 'Dinner with Jane at 6pm', 'John D.'),
-  ];
-  constructor() {}
+export class MessageListComponent implements OnInit {
+  messages: Message[] = [];
 
-  ngOnInit(): void {}
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messages = this.messageService.getMessages();
+  }
 
   onAddMessage(message: Message) {
     this.messages.push(message);
