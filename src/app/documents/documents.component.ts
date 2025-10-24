6c9d44813dd4b6d3e8cb13c7c07cbae01c5d0ad6
sadiebybee <byb22004@byui.edu>
2025-10-23 20:02:55 -0600

    document

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from './document.model';
+import { DocumentService } from './document.service';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
+  styleUrls: ['./documents.component.css'],
 })
-export class DocumentsComponent {
-  selectedDocument!: Document;
+export class DocumentsComponent implements OnInit {
+  selectedDocument: Document | null = null;
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit(): void {
+    this.documentService.documentSelectedEvent.subscribe(
+      (document: Document) => {
+        this.selectedDocument = document;
+      }
+    );
+  }
 }
