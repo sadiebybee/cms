1378bdcaa173587d6acbd5386c062c74de616c83
sadiebybee <byb22004@byui.edu>
2025-10-24 19:00:14 -0600

    routerhtml

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,13 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
-
-  onSelected(selectedFeature: string) {
-    this.selectedFeatureEvent.emit(selectedFeature);
-  }
-}
+export class HeaderComponent { }
